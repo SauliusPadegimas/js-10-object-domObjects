b0a6a3ac31678d4edb215cea72ca958c2c5b3b29
SauliusPadegimas <saulius.padegimas@gmail.com>
2022-07-19 15:32:15 +0300

    objekto metodai

--- js/main.js
+++ js/main.js
@@ -1,2 +1,45 @@
 'use strict';
 console.log('main.js');
+
+const user1 = {
+    name: 'John',
+    age: 21,
+    town: 'London',
+    hasCar: true,
+    hi: function() {
+        console.log(`Sveiki as esu ${this.name} is ${this.town}`)
+    }
+};
+user1.hi();
+
+const marks1 = {
+    marks: [4, 10, 7, 10],
+    avg: function() {
+        // atspausdinti ir grazinti vidurki
+        // for (kintamasis of masyvas/iterable)
+        let sum = 0;
+        for (const nr of this.marks) {
+            sum += nr;
+        }
+        console.log('sum ===', sum);
+        const avg = sum / this.marks.length
+        console.log('avg ===', avg.toFixed(2));
+        return avg.toFixed(2);
+    },
+    max: function() {
+        // atspausdinti ir grazinti didziausia skaiciu
+        const marksSorted = this.marks.slice(); //padaro masyvo kopija, kad sort nepakeistu originalo
+        marksSorted.sort(function(a, b){return b-a}); //rikiuoja skaitini masyva. funkcija pakeicia originala
+        console.log('Max ===', marksSorted[0]);
+        return marksSorted[0];
+    },
+    addMark: function(newMark) {
+        console.log(`Adding ${newMark} to marks`)
+        this.marks.push(newMark);
+    },
+}
+
+marks1.addMark(8);
+marks1.addMark(10);
+marks1.avg();
+marks1.max();
